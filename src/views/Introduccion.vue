f21a31c9c972e435a2a2202f79ed1ed51d1c595c
Andrea Botello <andrea.botellor@misena.edu.co>
2025-09-26 12:11:12 -0500

    Video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -28,8 +28,8 @@
             .col-lg-12
               figure
                 .video
-                  iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-              figcaption Video. 
+                  iframe(width="560" height="315" src="https://www.youtube.com/embed/_uAgrJLePbU?si=fViU-GKYAlz0QTfg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+              figcaption Video. Ingresos y gastos - Introducción
 
 </template>
 

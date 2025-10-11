3f53bc0d9e9c3cea5f58f8048c3107c58378a90a
Andrea Botello <andrea.botellor@misena.edu.co>
2025-10-10 21:02:19 -0500

    Video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -28,7 +28,7 @@
             .col-lg-12
               figure
                 .video
-                  iframe(width="560" height="315" src="https://www.youtube.com/embed/Js4FGhk8_pM?si=fxlkNKMKlbZtA2Nl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+                  iframe(width="560" height="315" src="https://www.youtube.com/embed/8bvoREaRhP8?si=ViGoCY_LO20lpO0u" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
               figcaption Video. Gestión eficiente de ingresos y gastos
 
 
